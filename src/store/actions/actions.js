1c3337fc139bdd55f4b4e0129a6c825f50d6b8a6
troy <troyflavel@gmail.com>
2021-03-20 01:01:42 +1300

    add async with redux-thunk

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,47 +1,54 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE_RESULT = 'STORE_RESULT';
-export  const DELETE_RESULT = 'DELETE_RESULT';
-
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const ADD = "ADD";
+export const SUBTRACT = "SUBTRACT";
+export const STORE_RESULT = "STORE_RESULT";
+export const DELETE_RESULT = "DELETE_RESULT";
 
 export const increment = () => {
-    return {
-        type: INCREMENT
-    }
-}
+  return {
+    type: INCREMENT,
+  };
+};
 
 export const decrement = () => {
-    return {
-        type: DECREMENT
-    }
-}
+  return {
+    type: DECREMENT,
+  };
+};
 
 export const add = (val) => {
-    return {
-        type: ADD,
-        val: val
-    }
-}
+  return {
+    type: ADD,
+    val: val,
+  };
+};
 
 export const subtract = (val) => {
-    return {
-        type: SUBTRACT,
-        val: val
-    }
-}
+  return {
+    type: SUBTRACT,
+    val: val,
+  };
+};
+
+export const save_result = (result) => {
+  return {
+    type: STORE_RESULT,
+    result: result,
+  };
+};
 
 export const store_result = (result) => {
-    return {
-        type: STORE_RESULT,
-        result: result
-    }
-}
+  return (dispatch) => {
+    setTimeout(() => {
+        dispatch(save_result(result))
+    }, 2000);
+  };
+};
 
 export const delete_result = (id) => {
-    return {
-        type: DELETE_RESULT,
-        resElId: id
-    }
-}
+  return {
+    type: DELETE_RESULT,
+    resElId: id,
+  };
+};
